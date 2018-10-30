5f2ab6c89d4df71524dc783e001182365c344524
talbs <duncan_talbot@hotmail.com>
2018-10-30 21:42:12 +1100

    Passport Facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/conFusion'
+    'mongoUrl': 'mongodb://localhost:27017/conFusion',
+    'facebook': {
+        clientId: '352965438601760',
+        clientSecret: 'b1570417146f11bd6f7838bf30abb997'
+    }
 }
